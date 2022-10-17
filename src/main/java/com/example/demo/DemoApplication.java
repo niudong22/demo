e7db1ae5af31727f5ef08fad3acd207e6c95751e
liudong <285062900@qq.com>
2022-10-17 14:55:26 +0800

    first

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(DemoApplication.class, args);
+		try {
+			System.out.println(1);
+			throw new ArrayIndexOutOfBoundsException("xx");
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+			System.out.println(2);
+		}finally {
+			System.out.println(3);
+		}
+	}
+
+}
