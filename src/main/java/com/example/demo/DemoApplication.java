88091775fe53eee6ac8b5d49fac84c69f962eb5f
liudong <285062900@qq.com>
2022-10-17 15:10:52 +0800

    master commit 1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,10 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 //		SpringApplication.run(DemoApplication.class, args);
 		try {
-			System.out.println(1);
-			System.out.println(1);
-			System.out.println(1);
-			System.out.println(1);
+			System.out.println("主分支");
 			throw new ArrayIndexOutOfBoundsException("xx");
 		} catch (NullPointerException e) {
 			e.printStackTrace();
